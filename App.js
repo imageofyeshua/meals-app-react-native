a662a06f43ba3db415975be5f3b8f6cd95f4ea5e
Daniel Park <intheimageofyeshua@gmail.com>
2024-12-15 21:12:05 +0900

    Displayed Meals

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import { StyleSheet } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
+import { StyleSheet } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style='dark' />
+      <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="MealsCategories">
           <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
         </Stack.Navigator>
@@ -22,6 +22,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  container: {},
 });
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,20 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, FlatList, View, Text } from "react-native";
+import React from "react";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen = () => {
+const MealsOverviewScreen = ({ route }) => {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default MealsOverviewScreen
+export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16
-  }
-})
+    padding: 16,
+  },
+});
