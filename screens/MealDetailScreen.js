f8536a4d7b08226d5e8de02395cf0ce4912f6629
Daniel Park <intheimageofyeshua@gmail.com>
2024-12-16 21:27:52 +0900

    Created Meal Details Component

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,16 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
 
-const MealDetailScreen = ({route}) => {
+const MealDetailScreen = ({ route }) => {
   const mealId = route.params.mealId;
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
     <View>
-      <Text>This is the Meal: {mealId}</Text>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>Steps</Text>
+      {selectedMeal.steps.map((step) => (
+        <Text key={step}>{step}</Text>
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default MealDetailScreen
+export default MealDetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
